62c146636c673018b87a0530d2f9c38b20039bc6
Cosmina23 <cosmina.tunea@student.upt.ro>
2024-04-12 22:17:56 +0300

    setting verification for password/email at login

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,5 +1,9 @@
 import React,{ useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import '../Css/Login.css';
+
+import Icon from '../resources/Android.png'
+import Logo from '../resources/Logo.png'
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -7,10 +11,12 @@ const Login = () => {
     const [redirect, setRedirect] = useState(false);
     const navigate = useNavigate();
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     const submit = async (e) => {
         e.preventDefault();
         
-            await fetch(' http://localhost:5269/api/Login', {
+            const response = await fetch(' http://localhost:5269/api/Login', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 credentials: 'include',//for cookies
@@ -21,7 +27,12 @@ const Login = () => {
             });
             //if backend worked
 
-            setRedirect(true);
+            if (response.ok) {
+                setRedirect(true);
+            } else {
+                // Dacă autentificarea a eșuat, afișează mesajul de eroare
+                setErrorMessage('Email sau parolă incorecte. Te rugăm să încerci din nou.');
+            }
 
     }
     
@@ -30,24 +41,40 @@ const Login = () => {
     }
 
     return(
-        <form onSubmit={submit}>
-        <h1>Conectare</h1>
-                <div>
-                    <input type='text' className='email_class' autoComplete="EMAIL" placeholder='Email' required
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                    <input type='text' className='par_class' autoComplete="PAROLA" placeholder='Parola' required
-                        onChange={e => setParola(e.target.value)}
-                    />
-                   
-                    <button type='submit'>Login</button>
+        <div className='main-container main-register'>
+            <div className='start-container'>
+                <div className='container1'>
+                    <form onSubmit={submit}>
+                        <div className=' start-creare'>
+                            <div>
+                                <img src={Icon} alt='Imagine' className='img1'/>
+                            </div>
+                            <h1 className='titlu_creare_cont'>Conectare</h1>
+                            <div className='inputs-login'>
+                                <input type='text' className='email_class' autoComplete="EMAIL" placeholder='Email' required
+                                onChange={e => setEmail(e.target.value)}
+                                />
+                                <input type='text' className='par_class' autoComplete="PAROLA" placeholder='Parola' required
+                                onChange={e => setParola(e.target.value)}
+                                />
+                                {errorMessage && <p className="error-message">{errorMessage}</p>}
+                                <div className='creare_cont_btn'>
+                                    <button type='submit'>Login</button>
+                                </div>
+                            </div>
+                            <div>
+                                <p>
+                                    Nu ai cont? <Link to="/register">Creaza unul</Link>
+                                </p>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div>
-                    <p>
-                        Nu ai cont? <Link to="/register">Creeaza unul</Link>
-                    </p>
+                <div className='img-class'>
+                    <img src={Logo} alt='Imagine' className='img2'/>
                 </div>
-        </form>
+            </div>
+        </div>
     )
 }
 
