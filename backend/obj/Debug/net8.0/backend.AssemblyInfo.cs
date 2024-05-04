85bbbd3d8568dbd73fb8b9af1a6a25d71c759c09
nico-stef <nico.stefanica12@gmail.com>
2024-05-04 12:13:06 +0300

    a

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49dbc1b47e496f47296aed67b76e3944cfb6991c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26b7ddbe650b718027c927d501644b9b43beae05")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
